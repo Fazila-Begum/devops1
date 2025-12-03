10085ec9eb870496b0add305988e6b9a2b62e691
Fazila-Begum <116628967+Fazila-Begum@users.noreply.github.com>
2025-12-03 10:59:01 +0530

    first commit

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def submit():
     year=request.form['year']
     return render_template('result.html',name=username,mail=email,roll=rollno,yr=year)
 if __name__=="__main__":
-    app.run(host='0.0.0.0',port=5000,debug=True)
+    app.run(debug=True)
